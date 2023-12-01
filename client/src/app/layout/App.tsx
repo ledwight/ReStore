c7f07328bd7b675e27c81a5a47d37e91d084e494
Dwight Le <ledwight@gmail.com>
2023-11-30 22:38:09 -0500

    Added routing and product details

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
-import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false)
@@ -20,7 +20,7 @@ function App() {
             <CssBaseline />
             <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
             <Container>
-                <Catalog />
+                <Outlet />
             </Container>
         </ThemeProvider>
     );
